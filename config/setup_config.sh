0ab7c39a82d1ff8edb8ab9e9182c26d379a4d4b6
William Ellett <w.ellett@tektindustries.com>
2021-07-17 00:31:26 +1000

    Customise set up script to work with my repo and clean up backup if empty

--- config/setup_config.sh
+++ config/setup_config.sh
@@ -1,18 +1,24 @@
 #! /bin/bash
 # Basic installation script to automate configuration set-up
-# Currently just ripped straight from Atlassian
+# Currently ripped almost straight from Atlassian
 
-git clone --bare https://bitbucket.org/durdn/cfg.git $HOME/.cfg
+configURL="https://github.com/Wellett/configs/blob/master/config/setup_config.sh"
+
+git clone --bare $configURL $HOME/.config
+
 function config {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir=$HOME/.config/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup
+mkdir -p config_backup
 config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} config_backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
+
+# Remove config_backup if empty
+rmdir --ignore-fail-on-non-empty config_backup 
